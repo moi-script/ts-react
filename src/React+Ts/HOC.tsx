68c035772c3109ff8712b1885b66cc56fd0776e6
lowkeyCodee <rizskplay@gmail.com>
2026-03-11 12:19:20 +0800

    HOC permission

--- src/React+Ts/HOC.tsx
+++ src/React+Ts/HOC.tsx
@@ -1,34 +1,29 @@
-import React from 'react';
+import React from "react";
 
 // 1. Define the props the HOC will "inject" or handle
 interface WithLoadingProps {
   isLoading: boolean;
 }
 
-// 2. The HOC function
-// T is the props of the original component
 export function withLoading<T extends object>(
-  Component: React.ComponentType<T>
+  Component: React.ComponentType<T>,
 ) {
-  // 3. Return a new component
   return (props: T & WithLoadingProps) => {
     const { isLoading, ...remainingProps } = props;
-
     if (isLoading) return <div>Loading...</div>;
 
-    // We cast remainingProps back to T to satisfy the original component
     return <Component {...(remainingProps as T)} />;
   };
 }
 
-
-
 interface UserProps {
-  role: 'admin' | 'user';
+  role: "admin" | "user";
 }
-function withAdminAuth<T extends object>(WrappedComponent: React.ComponentType<T>) {
+function withAdminAuth<T extends object>(
+  WrappedComponent: React.ComponentType<T>,
+) {
   return (props: T & UserProps) => {
-    if (props.role !== 'admin') {
+    if (props.role !== "admin") {
       return <p>Access Denied. Admins only.</p>;
     }
 
@@ -40,4 +35,57 @@ const Dashboard = () => <div>Sensitive Admin Data</div>;
 const AdminDashboard = withAdminAuth(Dashboard);
 
 // In your App
-<AdminDashboard role="user" /> // Shows "Access Denied"
+<AdminDashboard role="user" />; // Shows "Access Denied"
+
+// With Permision
+type UserRole = "admin" | "editor" | "viewer";
+
+// This is the hierarchy logic
+const roles: Record<UserRole, number> = {
+  admin: 3,
+  editor: 2,
+  viewer: 1,
+};
+
+export function withPermission<T extends object>(
+  Component: React.ComponentType<T>,
+  requiredRole: UserRole,
+) {
+  return (props: T & { userRole: UserRole }) => {
+    // 1. Get the rank of the current userRole from props
+    const { userRole } = props;
+
+    if (roles[requiredRole] >= roles[userRole]) {
+      return <h1>Permission accepted</h1>;
+    }
+
+    return <p>Permission Denied</p>; // Change this!
+  };
+}
+
+// --- TEST CASE ---
+
+const SensitiveData = ({ title }: { title: string }) => (
+  <div style={{ padding: "20px", border: "1px solid red" }}>
+    <h3>{title}</h3>
+    <p>This is highly sensitive information!</p>
+  </div>
+);
+
+// Wrap the component
+const ProtectedData = withPermission(SensitiveData, "admin");
+
+export default function App() {
+  return (
+    <div>
+      <h1>Permissions Test</h1>
+      {/* This should show the message "Denied" */}
+      <ProtectedData title="Q1 Revenue" userRole="viewer" />
+
+      <hr />
+
+      {/* This should show the SensitiveData component */}
+      <ProtectedData title="Q1 Revenue" userRole="admin" />
+    </div>
+  );
+}
